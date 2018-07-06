29467dade051e301143f7b007023780e2c17613b
Tamrat <tamrat.baruda@gmail.com>
2018-07-06 00:31:06 -0400

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -18,7 +18,7 @@ class Solution {
         return addTwoNumbers(l1, l2, 0);
     }
     
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2, int carryOver) {
+    private ListNode addTwoNumbers(ListNode l1, ListNode l2, int carryOver) {
     	if (l1 == null && l2 == null && carryOver == 0){
             return null;
         }
@@ -29,18 +29,32 @@ class Solution {
             return l1;
         }
         else {
-            int val1 = (l1 != null)? l1.val : 0;
-            int val2 = (l2 != null)? l2.val : 0;
-        	int sum = val1 + val2 + carryOver ;
+            int value1 = getValue(l1);
+            int value2 = getValue(l2);
+            
+        	int sum = value1 + value2 + carryOver ;
             int remainder = sum % 10;
             carryOver = sum / 10;
             ListNode result = new ListNode(remainder );
             
-            l1 = (l1 != null)? l1.next : l1;
-            l2 = (l2 != null)? l2.next : l2;
-            result.next = addTwoNumbers(l1, l2, carryOver);
+            result.next = addTwoNumbers(getNextNode(l1), getNextNode(l2), carryOver);
             return result;
         }
     }
+    
+    private int getValue(ListNode node){
+        int value = 0;
+        if (node != null){
+            value = node.val;
+        }
+        return value;
+    }
+    
+    private ListNode getNextNode(ListNode node){
+        if (node != null){
+             node = node.next;
+        }
+        return node;
+    } 
 
 }
